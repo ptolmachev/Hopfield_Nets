9029e3b1e9f0252a4ceb483a10d6107151c50db7
Pavel <betadecay1993@gmail.com>
2019-09-04 14:14:54 +1000

    Working version of Boltzmann Machines and Hopfield Nets

--- src/Hopfield_net.py
+++ src/Hopfield_net.py
@@ -13,66 +13,93 @@ def introduce_random_flips(pattern, k):
 def sigmoid(x,T):
     return 1.0/(1 + np.exp(-x/T))
 
-class Hopfield_net():
-    def __init__(self, num_neurons, T_max, T_decay, p):
+
+class Hopfield_network():
+    def __init__(self, num_neurons, p):
         self.num_neurons = num_neurons
-        self.weights = (1/self.num_neurons)*np.random.randn(self.num_neurons,self.num_neurons)
+        Y = np.random.randn(self.num_neurons,self.num_neurons)
+        R = (Y+Y.T)/2
+        R[np.arange(num_neurons),np.arange(num_neurons)] = np.zeros(num_neurons)
+        self.weights = (1/self.num_neurons)*R
+        self.biases = np.zeros(num_neurons)
         self.state = np.array([random_activity(p) for i in range(self.num_neurons)]).reshape(1,-1)
-        self.learned_patterns = None
-        self.T = T_max
-        self.T_max = T_max
-        self.T_decay = T_decay
 
-    def update_state(self):
-        h = np.dot(self.weights,self.state.reshape(-1,1))
-        self.state = np.array([(1 if np.random.rand() < h[i] else -1) for i in range(len(h))])
-        return None
-
-    def evolve(self):
-        while self.T > 0.001:
-            self.update_state()
-            self.T *= self.T_decay
+    def update_state(self, sync=True):
+        if sync == True:
+            h = (np.dot(self.weights,self.state.reshape(-1,1)).T + self.biases).flatten()
+            self.state = np.sign(h)
+        elif sync == False:
+            for i in range(num_neurons):
+                h_i = np.dot(self.weights[i,:], self.state)
+                self.state[i] = np.sign(h_i)
+        else:
+            raise AttributeError('sync variable can take only boolean values')
         return None
 
     def learn_patterns(self, patterns):
         patterns = np.array(patterns)
+        Y = np.dot(patterns.T, patterns)
+        Y[np.arange(self.num_neurons),np.arange(self.num_neurons)] = np.zeros(self.num_neurons)
         self.weights = (1/self.num_neurons)*np.dot(patterns.T, patterns)
-        self.learned_patterns = patterns
         return None
-
-    def retrieve_pattern(self, new_state):
-        self.T = self.T_max
-        self.state = new_state
-        while self.T > 0.001:
-            self.update_state()
-            self.T *= self.T_decay
-        return self.state
 
     def set_weights(self, new_weights):
         self.weights = new_weights
         return None
 
+    def retrieve_pattern(self, new_state, sync, time):
+        self.state = new_state
+        t = 0
+        while t < time:
+            self.update_state(sync)
+            t += 1
+        return self.state
+
+class Boltzmann_machine(Hopfield_network):
+    def __init__(self, num_neurons, T_max, T_decay, p):
+        super().__init__(num_neurons, p)
+        self.T = T_max
+        self.T_max = T_max
+        self.T_decay = T_decay
+
+    def update_state(self, sync=True):
+        if sync == True:
+            h = (np.dot(self.weights, self.state.reshape(-1, 1)).T + self.biases).flatten()
+            self.state = np.array([(1 if np.random.rand() < h[i] else -1) for i in range(len(h))])
+        elif sync == False:
+            for i in range(num_neurons):
+                h_i = np.dot(self.weights[i,:], self.state)
+                self.state[i] = 1 if np.random.rand() < h_i else -1
+        else:
+            raise AttributeError('sync variable can take only boolean values')
+        return None
+
+    def retrieve_pattern(self, new_state, sync, time = None):
+        self.T = self.T_max
+        self.state = new_state
+        while self.T > 0.0001:
+            self.update_state(sync)
+            self.T *= self.T_decay
+        return self.state
 
 if __name__ == '__main__':
     num_neurons = 100
-    HN = Hopfield_net(num_neurons=num_neurons, T_max=1, T_decay = 0.999, p=0.5)
-
-    # #np.array([random_activity(0.5) for i in range(num_neurons)])#
+    sync = False
+    HN = Hopfield_network(num_neurons=num_neurons, p=0.5)
+    # HN = Boltzmann_machine(num_neurons=num_neurons,T_max=10, T_decay=0.99, p=0.5)
     pattern1 = np.array([(-1)**i for i in range(num_neurons)])
-    pattern2 = np.hstack([-1.0*np.ones(num_neurons//2),np.ones(num_neurons//2)])
-    pattern3 = np.hstack([((-1.0)**i)*np.ones(num_neurons//5) for i in range(5)])
+    pattern2 = np.hstack([((-1)**i)*np.ones(num_neurons//50) for i in range(50)])
+    pattern3 = np.hstack([-1.0*np.ones(num_neurons//2),np.ones(num_neurons//2)])
+    HN.learn_patterns([pattern1, pattern2, pattern3])
 
-    HN.learn_patterns([pattern1, pattern2])
-
-    pattern_r = introduce_random_flips(pattern2, 10)
+    pattern_r = introduce_random_flips(pattern1, 10)
 
     print('Similarity with different patterns (before): ')
     print(np.linalg.norm(pattern_r - pattern1, 2))
     print(np.linalg.norm(pattern_r - pattern2, 2))
     print(np.linalg.norm(pattern_r - pattern3, 2))
 
-    retrieved_pattern = HN.retrieve_pattern(pattern_r)
-
+    retrieved_pattern = HN.retrieve_pattern(pattern_r, sync, 100)
     print('Similarity with different patterns (after): ')
     print(np.linalg.norm(retrieved_pattern - pattern1, 2))
     print(np.linalg.norm(retrieved_pattern - pattern2, 2))
