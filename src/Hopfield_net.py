c7c9b5ec1905defffeae350fd8f508b15c088a8e
Pavel <betadecay1993@gmail.com>
2019-09-05 13:58:00 +1000

    Fixed an implementation of continuous Hopfield Nets

--- src/Hopfield_net.py
+++ src/Hopfield_net.py
@@ -21,16 +21,17 @@ class Hopfield_network():
         R = (Y+Y.T)/2
         R[np.arange(num_neurons),np.arange(num_neurons)] = np.zeros(num_neurons)
         self.weights = (1/self.num_neurons)*R
+        self.biases = np.zeros(self.num_neurons)
         self.hidden_state = np.random.rand(num_neurons)-0.5
         self.state = np.sign(self.hidden_state)
 
     def update_state(self, sync=True):
         if sync == True:
-            self.hidden_state = (np.dot(self.weights,self.state.reshape(-1,1))).flatten()
+            self.hidden_state = np.matmul(self.weights, self.state) + self.biases
             self.state = np.sign(self.hidden_state)
         elif sync == False:
             for i in range(num_neurons):
-                self.hidden_state[i] = np.dot(self.weights[i,:], self.state)
+                self.hidden_state[i] = np.dot(self.weights[i,:], self.state) + self.biases[i]
                 self.state[i] = np.sign(self.hidden_state[i])
         else:
             raise AttributeError('sync variable can take only boolean values')
@@ -43,15 +44,19 @@ class Hopfield_network():
             # deprecate self-connectivity
             Y[np.arange(self.num_neurons),np.arange(self.num_neurons)] = np.zeros(self.num_neurons)
             self.weights = (1/self.num_neurons)*np.dot(patterns.T, patterns)
+            # self.biases += 0.05*np.mean(patterns, axis = 0)
+
         elif rule =='Storkey':
-            # self.weights = np.zeros((self.num_neurons, self.num_neurons))
             for i in range(patterns.shape[0]):
                 pattern = patterns[i]
-                h = (np.dot(self.weights, pattern.reshape(-1,1)).T).flatten() - (np.diag(self.weights)*pattern)
+                h = np.matmul(self.weights, pattern) - (np.diag(self.weights)*pattern) + self.biases
                 H = np.hstack([h.reshape(-1,1) for j in range(self.num_neurons)]) - self.weights*pattern
                 pattern_matrix = np.hstack([pattern.reshape(-1,1) for j in range(self.num_neurons)])
                 Z = pattern_matrix - H
                 self.weights += (1/self.num_neurons)*(Z*Z.T)
+                # self.biases += 0.1*pattern
+        else:
+            raise ValueError('the parameter rule can only take values \'Hebb\' or \'Storkey\'')
         return None
 
     def set_weights(self, new_weights):
@@ -75,7 +80,7 @@ class Boltzmann_machine(Hopfield_network):
 
     def update_state(self, sync=True):
         if sync == True:
-            self.hidden_state = (np.dot(self.weights,self.state.reshape(-1,1))).flatten()
+            self.hidden_state = np.matmul(self.weights,self.state) + self.biases
             h_tmp = sigmoid(self.hidden_state, self.T)
             self.state = np.array([(1 if np.random.rand() < h_tmp[i] else -1) for i in range(len(h_tmp))])
         elif sync == False:
@@ -103,24 +108,25 @@ class Continuous_HN(Hopfield_network):
 
     def update_state(self, sync=True):
         if sync == True:
-            self.hidden_state = (np.dot(self.weights,self.state.reshape(-1,1))).flatten()
-            self.state = sigmoid(-(1-self.alpha) * self.state + self.hidden_state, self.T)
+            self.hidden_state += self.alpha*(-self.hidden_state + np.matmul(self.weights,self.state) + self.biases)
+            self.state = 2*sigmoid(self.hidden_state, self.T) - 1
         else:
             for i in range(self.num_neurons):
-                self.hidden_state[i] = np.dot(self.weights[i,:], self.state)
-                self.state[i] = sigmoid(-(1 - self.alpha) * self.state[i] + self.hidden_state[i], self.T)
+                self.hidden_state[i] += self.alpha * (-self.hidden_state[i] + np.dot(self.weights[i,:], self.state) + self.biases[i])
+                self.state[i] = 2*sigmoid(self.hidden_state[i], self.T) - 1
         return None
 
 if __name__ == '__main__':
     num_neurons = 100
-    num_patterns = 12
+    num_patterns = 20
     sync = False
+    rule = 'Storkey'
     flips = 25
-    HN = Hopfield_network(num_neurons=num_neurons, p=0.5)
-    # HN = Continuous_HN(num_neurons=num_neurons, T=0.000001, alpha=1, p=0.5)
+    # HN = Hopfield_network(num_neurons=num_neurons, p=0.5)
     # HN = Boltzmann_machine(num_neurons=num_neurons,T_max=0.1, T_decay=0.99, p=0.5)
+    HN = Continuous_HN(num_neurons=num_neurons, T=0.001, alpha=0.99, p=0.5)
     patterns = [random_state(0.5, num_neurons) for i in range(num_patterns)]
-    HN.learn_patterns(patterns, rule='Storkey')
+    HN.learn_patterns(patterns, rule=rule)
 
     pattern_r = introduce_random_flips(patterns[0], flips)
 
@@ -132,3 +138,4 @@ if __name__ == '__main__':
     print('Similarity with different patterns (after): ')
     for i in range(len(patterns)):
         print(np.linalg.norm(retrieved_pattern - patterns[i], 2))
+
