d1e983379f0edf470ec6324dafecc9a1fe89f262
Pavel <betadecay1993@gmail.com>
2019-09-06 16:37:21 +1000

    Found good parameters

--- src/Hopfield_net.py
+++ src/Hopfield_net.py
@@ -41,13 +41,14 @@ class Hopfield_network():
     def learn_patterns(self, patterns, rule='Hebb'):
         patterns = np.array(patterns).squeeze()
         if rule == 'Hebb':
+            # self.biases = 1.0*np.mean(patterns, axis = 0)
             Y = np.dot(patterns.T, patterns)
             # deprecate self-connectivity
             Y[np.arange(self.num_neurons),np.arange(self.num_neurons)] = np.zeros(self.num_neurons)
             self.weights = (1/self.num_neurons)*Y
-            # self.biases += 0.05*np.mean(patterns, axis = 0)
 
         elif rule =='Storkey':
+            # self.biases = 1.0*np.mean(patterns, axis = 0)
             for i in range(patterns.shape[0]):
                 pattern = patterns[i]
                 h = np.matmul(self.weights, pattern) - (np.diag(self.weights)*pattern) + self.biases
@@ -55,7 +56,12 @@ class Hopfield_network():
                 pattern_matrix = np.hstack([pattern.reshape(-1,1) for j in range(self.num_neurons)])
                 Z = pattern_matrix - H
                 self.weights += (1/self.num_neurons)*(Z*Z.T)
-                # self.biases += 0.1*pattern
+
+        elif rule == 'associative':
+            # self.biases = 1.0 * np.mean(patterns, axis=0)
+            Y = np.matmul(patterns.T, np.linalg.pinv(patterns.T))
+            Y[np.arange(self.num_neurons), np.arange(self.num_neurons)] = np.zeros(self.num_neurons)
+            self.weights = (1/self.num_neurons) * Y
         else:
             raise ValueError('the parameter rule can only take values \'Hebb\' or \'Storkey\'')
         return None
@@ -134,11 +140,11 @@ class Continuous_HN(Hopfield_network):
 
     def update_state(self, sync=True):
         if sync == True:
-            self.hidden_state += self.alpha*(-self.hidden_state + np.matmul(self.weights,self.state) + self.biases)
+            self.hidden_state = self.hidden_state + self.alpha*(-self.hidden_state + np.matmul(self.weights,self.state) + self.biases)
             self.state = 2*sigmoid(self.hidden_state, self.T) - 1
         else:
             for i in range(self.num_neurons):
-                self.hidden_state[i] += self.alpha * (-self.hidden_state[i] + np.dot(self.weights[i,:], self.state) + self.biases[i])
+                self.hidden_state[i] = self.hidden_state[i] + self.alpha * (-self.hidden_state[i] + np.dot(self.weights[i,:], self.state) + self.biases[i])
                 self.state[i] = 2*sigmoid(self.hidden_state[i], self.T) - 1
         self.fr = (self.state + 1) / 2
         return None
@@ -167,29 +173,35 @@ class Continuous_HN_adaptive(Hopfield_network):
 
 if __name__ == '__main__':
     num_neurons = 150
-    num_patterns = 40
+    num_patterns = 30
     sync = True
-    rule = 'Storkey'
+    rule = 'Storkey' #'Storkey'
     flips = 40
+    time = 500
     # HN = Hopfield_network(num_neurons=num_neurons, p=0.5)
     # HN = Boltzmann_machine(num_neurons=num_neurons,T_max=0.1, T_decay=0.99, p=0.5)
-    # HN = Continuous_HN_adaptive(num_neurons=num_neurons, T=0.05, alpha=0.25, p=0.5)
-    HN = Continuous_HN_adaptive(num_neurons=num_neurons, T=0.05, alpha=0.25, beta = 0.01, p=0.5)
+    # HN = Continuous_HN(num_neurons=num_neurons, T=0.000000001, alpha=0.99, p=0.5)
+    HN = Continuous_HN_adaptive(num_neurons=num_neurons, T=0.2, alpha=0.1, beta = 0.00, p=0.5)
     patterns = [random_state(0.5, num_neurons) for i in range(num_patterns)]
     HN.learn_patterns(patterns, rule=rule)
 
-    pattern_r = introduce_random_flips(patterns[1], flips)
+    pattern_r = introduce_random_flips(patterns[0], flips)
 
     print('\nSimilarity with different patterns (before): ')
     for i in range(len(patterns)):
         print(np.linalg.norm(pattern_r - patterns[i], 2))
 
-    retrieved_pattern, data = HN.retrieve_pattern(pattern_r, sync, 500, record = True)
+    retrieved_pattern, data = HN.retrieve_pattern(pattern_r, sync, time, record = True)
     print('\nSimilarity with different patterns (after): ')
     for i in range(len(patterns)):
         print(np.linalg.norm(retrieved_pattern - patterns[i], 2))
 
+    fig1 = plt.figure()
     plt.plot(data['hidden_variables'].T)
+    plt.show()
+    fig3 = plt.figure()
+    similarities = np.array([[np.dot(data['state'][:,j],patterns[i])/num_neurons for i in range(len(patterns))] for j in range(time)])
+    plt.plot(similarities)
     plt.show()
     # print('\nSimilarity of signs with different patterns (after): ')
     # for i in range(len(patterns)):
--- src/experiments.py
+++ src/experiments.py
@@ -2,7 +2,7 @@ from src.Hopfield_net import Hopfield_network, sigmoid, random_state, introduce_
 import numpy as np
 from matplotlib import pyplot as plt
 
-class Continuous_HN_adaptive(Hopfield_network):
+class Continuous_HN_oscillatory(Hopfield_network):
     def __init__(self, num_neurons, T, alpha, beta, p):
         super().__init__(num_neurons, p)
         self.T = T
@@ -27,6 +27,8 @@ class Continuous_HN_adaptive(Hopfield_network):
     def learn_patterns(self, patterns, rule='Hebb'):
         patterns = np.array(patterns).squeeze()
         if rule == 'Hebb':
+            self.biases = 1.*np.mean(patterns, axis = 0)
+
             Y = np.dot(patterns.T, patterns)
             # deprecate self-connectivity
             Y[np.arange(self.num_neurons),np.arange(self.num_neurons)] = np.zeros(self.num_neurons)
@@ -35,23 +37,51 @@ class Continuous_HN_adaptive(Hopfield_network):
             next_patterns = np.roll(patterns, axis = 0, shift = -1)
             Y = np.matmul(next_patterns.T, patterns)
             Y[np.arange(self.num_neurons), np.arange(self.num_neurons)] = np.zeros(self.num_neurons)
-            self.weights += 0.005*(1 / self.num_neurons) * Y
+            self.weights += 0.02*(1 / self.num_neurons) * Y
+
+        elif rule == 'Storkey':
+            alpha = 0.98
+            self.biases = 1.0 * np.mean(patterns, axis=0)
+            next_patterns = np.roll(patterns, axis=0, shift=-1)
+            for i in range(patterns.shape[0]):
+                pattern = patterns[i]
+                h = np.matmul(self.weights, pattern) - (np.diag(self.weights)*pattern) + self.biases
+                H = np.hstack([h.reshape(-1,1) for j in range(self.num_neurons)]) - self.weights*pattern
+                pattern_matrix = np.hstack([pattern.reshape(-1,1) for j in range(self.num_neurons)])
+                Z = pattern_matrix - H
+                self.weights += alpha*(1/self.num_neurons)*(Z*Z.T)
+
+                next_pattern = next_patterns[i]
+                h_next = np.matmul(self.weights, next_pattern) - (np.diag(self.weights)*next_pattern) + self.biases
+                H_next = np.hstack([h_next.reshape(-1,1) for j in range(self.num_neurons)]) - self.weights*next_pattern
+                pattern_matrix_next = np.hstack([next_pattern.reshape(-1,1) for j in range(self.num_neurons)])
+                Z_next = pattern_matrix_next - H_next
+                self.weights += (1-alpha)*(1/self.num_neurons)*(Z_next*Z.T)
+        elif rule == 'associative':
+            # self.biases = 1.0 * np.mean(patterns, axis=0)
+            next_patterns = np.roll(patterns, axis=0, shift=-1)
+            alpha = 0.9999
+            self.weights += (1 / self.num_neurons) * np.dot(alpha*patterns.T + (1-alpha)*next_patterns.T, np.linalg.pinv(patterns.T))
         else:
             raise ValueError('the parameter rule can only take values \'Hebb\' or \'Storkey\'')
         return None
 
 if __name__ == '__main__':
-    num_neurons = 10
-    num_patterns = 2
+    num_neurons = 20
+    num_patterns = 4
     sync = True
-    rule = 'Hebb'
+    rule = 'Storkey'
     flips = 0
-    time = 1000
-    HN = Continuous_HN_adaptive(num_neurons=num_neurons, T=0.3, alpha=1, beta=0.01, p=0.5)
+    time = 500000
+    HN = Continuous_HN_oscillatory(num_neurons=num_neurons, T=0.3, alpha=0.01, beta=0.000008, p=0.5)
     patterns = [random_state(0.5, num_neurons) for i in range(num_patterns)]
+    # patterns = []
+    # patterns.append(np.hstack([np.ones(8),np.zeros(4), np.zeros(4)]))
+    # patterns.append(np.hstack([np.zeros(4),np.ones(8), np.zeros(4)]))
+    # patterns.append(np.hstack([np.zeros(4), np.zeros(4), np.ones(8)]))
     HN.learn_patterns(patterns, rule=rule)
 
-    pattern_r = introduce_random_flips(patterns[1], flips)
+    pattern_r = introduce_random_flips(patterns[0], flips)
 
     print('\nSimilarity with different patterns (before): ')
     for i in range(len(patterns)):
@@ -62,15 +92,18 @@ if __name__ == '__main__':
     for i in range(len(patterns)):
         print(np.linalg.norm(retrieved_pattern - patterns[i], 2))
 
+
+
+    time_start = 200000
     fig1 = plt.figure()
-    plt.plot(data['hidden_variables'].T)
+    plt.plot(data['hidden_variables'].T[time_start:])
     plt.show()
 
     fig2 = plt.figure()
-    plt.plot(data['state'].T)
+    plt.plot(data['state'].T[time_start:])
     plt.show()
 
     fig3 = plt.figure()
-    similarities = np.array([[np.linalg.norm(data['state'][:,j] - patterns[i],2) for i in range(len(patterns))] for j in range(time)])
+    similarities = np.array([[np.dot(data['state'][:,time_start:][:,j],patterns[i])/num_neurons for i in range(len(patterns))] for j in range(time-time_start)])
     plt.plot(similarities)
     plt.show()
